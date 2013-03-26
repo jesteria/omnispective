9eba95253a5fb79e5db071efd500fcbad8986148
Jesse London <jesselondon@gmail.com>
2013-03-25 23:59:18 -0500

    made client django test runner use ManagementUtility rather than call_command for easy inheritance of argv

--- client/python/omniclient/django/tests/run.py
+++ client/python/omniclient/django/tests/run.py
@@ -1,12 +1,13 @@
 from django.conf import settings
-from django.core.management import call_command
+from django.core.management import ManagementUtility
 
 
 def main():
     settings.configure(
         TEST_RUNNER='base.NoDbNoseTestSuiteRunner',
+        INSTALLED_APPS=('django_nose',),
     )
-    call_command('test')
+    ManagementUtility().execute()
 
 
 if __name__ == '__main__':
